c8d03ec255ca97a36bf823553c298f713a8869f1
Zach Collins <recursive.cookie.jar@gmail.com>
2016-04-07 18:12:26 +0900

    Try fixing charset

--- src/webpack/static-sites/static-sites-entry.tsx
+++ src/webpack/static-sites/static-sites-entry.tsx
@@ -4,12 +4,13 @@ addRendering("index.html", `
 <!doctype html>
 <html>
   <head>
+    <meta charset="UTF-8">
     <meta httpEquiv="Content-Type" content="text/html; charset=UTF-8"/>
     <meta httpEquiv="x-ua-compatible" content="ie=edge"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
   </head>
   <body>
-    <script src="main.js"></script>
+    <script src="main.js" charset="UTF-8"></script>
   </body>
 </html>
 `);
